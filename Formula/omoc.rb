b55fdb6584475131e7f93607f688cfdfb3c727e6
finger <fingergohappy@163.com>
2026-02-28 16:02:49 +0800

    omoc 0.0.4

--- Formula/omoc.rb
+++ Formula/omoc.rb
@@ -1,8 +1,8 @@
 class Omoc < Formula
   desc "TUI tool for configuring Oh My OpenCode agent and category model assignments"
   homepage "https://github.com/fingergohappy/omoc"
-  url "https://github.com/fingergohappy/omoc/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "1bc986f4ea4a182f3eb071e0a45424d3e099912edfc90c06953d5cf55fcdd383"
+  url "https://github.com/fingergohappy/omoc/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "e86afa711c257341cca75e88e3d8bc72354faeda4309ddd81cc7428643731fc2"
   license "MIT"
 
   depends_on "go" => :build
