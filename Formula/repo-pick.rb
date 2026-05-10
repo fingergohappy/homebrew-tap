d835fcad8500ee50ed1d5daf58f65f997aed5dcb
finger <fingergohappy@163.com>
2026-05-10 21:24:49 +0800

    repo-pick v0.1.2

--- Formula/repo-pick.rb
+++ Formula/repo-pick.rb
@@ -1,25 +1,25 @@
 class RepoPick < Formula
   desc "TUI-only remote Git repository file and directory downloader"
   homepage "https://github.com/fingergohappy/repo-pick"
-  version "0.1.0"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.0/repo-pick_0.1.0_darwin_arm64.tar.gz"
-      sha256 "f68e76e45a9f2ce3283c1f0b918c9d306c284925e3c035ec43ab4f3129bef832"
+      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.2/repo-pick_0.1.2_darwin_arm64.tar.gz"
+      sha256 "0897e4947d999f2b002a0bb15635e465f7102f400ef993122ff70d9cd176ae41"
     else
-      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.0/repo-pick_0.1.0_darwin_amd64.tar.gz"
-      sha256 "d791daea5b81d2b01bed0590e40a1aba67c02436fed6b5e9564d38022ae2e9f4"
+      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.2/repo-pick_0.1.2_darwin_amd64.tar.gz"
+      sha256 "511b462f5bbac85c0d40d816252e87e0fbfc1e0cf8688a867be8c2d1832ad32d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.0/repo-pick_0.1.0_linux_arm64.tar.gz"
-      sha256 "e9d4941e14b7f7e63b747638f915638b1c63818735bdc423af505ca2c4c14c29"
+      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.2/repo-pick_0.1.2_linux_arm64.tar.gz"
+      sha256 "658266cffbb28437aad04911d0c96459a5f7788d7d7ad0633783a90378355dee"
     else
-      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.0/repo-pick_0.1.0_linux_amd64.tar.gz"
-      sha256 "16c0a7e1c9bb362f263318f50c8dba8b5cbf588ecd3b0543de5c591e3c34ceea"
+      url "https://github.com/fingergohappy/repo-pick/releases/download/v0.1.2/repo-pick_0.1.2_linux_amd64.tar.gz"
+      sha256 "89b0c1b713f1a61909ae8f3b123cb2e82dd62f2c98f16cef9ad708f394fb2652"
     end
   end
 
